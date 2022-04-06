745adc7378a2069614057c10bc8a57f54f3178b4
Akiraka <87097901+akiraka123@users.noreply.github.com>
2022-04-06 16:05:10 +0700

    tunjakan IDnya dan beberapa fitur tambahan

--- src/main.cpp
+++ src/main.cpp
@@ -16,10 +16,17 @@ int main(int argc, char** argv)
 
 	int menu_terpilih;
 	int pilihan;
+	string nama,nrp,npp,departemen,pendidikan,unit;
+	int dd,mm,yy,tahunmasuk;
+	int idm=0, idd=0, idt=0;
+	int noID=0;
+	cout << "\nMake By AKIRAKA \nSelamat datang di Universitas X" << endl << endl;
 
-	while(menu_terpilih != 7) {
+	while(1) {	
 		
-		cout << "Selamat datang di Universitas X" << endl << endl;
+		cout<< "ENTER untuk masuk ke menu utama  ";
+		cin.ignore();
+		cout << "\n\nSIAKAD\n                                                    "<< endl;
 		cout << "Data statistik:" << endl;
 		cout << "  1. Jumlah Mahasiswa             : " << recMhs.size() << " Mahasiswa" << endl;
 		cout << "  2. Jumlah Dosen                 : " << recDosen.size() << " Dosen" << endl;
@@ -32,17 +39,15 @@ int main(int argc, char** argv)
 		cout << "  4. Tampilkan semua Mahasiswa" << endl;
 		cout << "  5. Tampilkan semua Dosen" << endl;
 		cout << "  6. Tampilkan semua Tenaga Kependidikan" << endl;
-		cout << "  7. Keluar " << endl;
+		cout << "  7. Keluar \n" << endl;
 		cout << "-> Silahkan memilih salah satu: ";
 		cin >> menu_terpilih;
-
+		cout<< endl;
 		switch (menu_terpilih) 
 		{
 			case 1:
 			{
-			
-			string nama,nrp,departemen;
-			int dd,mm,yy,tahunmasuk,id;
+			++idm;
 			cout << "Masukkan Nama			: ";
 			cin >> nama;
 
@@ -65,14 +70,13 @@ int main(int argc, char** argv)
 			cin >> tahunmasuk;
 			cout<< endl;
 
-			mahasiswa maba(id,nama,dd,mm,yy,nrp,departemen,tahunmasuk);
+			mahasiswa maba(idm,nama,dd,mm,yy,nrp,departemen,tahunmasuk);
 			recMhs.push_back(maba);
 		}
-				break;
+		break;
 			case 2:
 			{
-				int id,dd,mm,yy;
-				string nama,npp,departemen,pendidikan;
+			++idd;	
 			cout << "Masukkan Nama			: ";
 			cin >> nama;
 
@@ -95,15 +99,13 @@ int main(int argc, char** argv)
 			cin >> pendidikan;
 			cout<< endl;
 
-				dosen dosenbaru(id,nama,dd,mm,yy,npp,departemen,pendidikan);
+				dosen dosenbaru(idd,nama,dd,mm,yy,npp,departemen,pendidikan);
 				recDosen.push_back(dosenbaru);
 			}
 				break;
 			case 3:
 			{
-				int id,dd,mm,yy;
-				string nama,npp,departemen,unit;
-
+			++idt;
 			cout << "Masukkan Nama			: ";
 			cin >> nama;
 
@@ -123,36 +125,46 @@ int main(int argc, char** argv)
 			cin >> unit;
 			cout<< endl;
 
-				tendik tendikbaru( id,nama,dd,mm,yy,npp,unit);
+				tendik tendikbaru(idt,nama,dd,mm,yy,npp,unit);
 				recTendik.push_back(tendikbaru);
 			}
 				break;
 
 			case 4:	
-			{	
+			{
 				for (int i = 0; i < recMhs.size(); i++)
 				{
-					cout<< "Nama Mahasiswa	: "<<recMhs[i].getNama()<<endl;
-					cout<< "Tanggal Lahir	: "<<recMhs[i].getTglLahir()<< ", "<< recMhs[i].getBulanLahir()<<", "<<recMhs[i].getTahunLahir()<<endl;
-					cout<< "NRP 	: "<<recMhs[i].getNRP()<<endl;
-					cout<< "Departemen	: "<<recMhs[i].getDepartemen()<<endl;
-					cout<< "Tahun Masuk	: "<<recMhs[i].getNama()<<endl;
-					cout << endl;
+					cout<<"\n"<<recMhs[i].getId()<< ". Nama Mahasiswa	: "<<recMhs[i].getNama()<<endl;
+					cout<< "   NRP 			: "<<recMhs[i].getNRP()<<"\n";
 				}
+				cout<< "masukkan nomer ID yang ingin dilihat : ";
+				cin>> noID;
+		
+					cout<<"\n\n"<<recMhs[noID-1].getId()<< ". Nama Mahasiswa	: "<<recMhs[noID-1].getNama()<<endl;
+					cout<< "   NRP 			: "<<recMhs[noID-1].getNRP()<<endl;
+					cout<< "   Tanggal Lahir	: "<<recMhs[noID-1].getTglLahir()<< ", "<< recMhs[noID-1].getBulanLahir()<<", "<<recMhs[noID-1].getTahunLahir()<<endl;
+					cout<< "   Departemen		: "<<recMhs[noID-1].getDepartemen()<<endl;
+					cout<< "   Tahun Masuk		: "<<recMhs[noID-1].getNama()<<endl;
+					cout << endl;
 			}
 				break;
 
 			case 5:
-			{
+			{	
 				for (int i = 0; i < recMhs.size(); i++)
 				{
-					cout<< "Nama Mahasiswa	: "<<recDosen[i].getNama()<<endl;
-					cout<< "Tanggal Lahir	: "<<recDosen[i].getTglLahir()<< ", "<< recDosen[i].getBulanLahir()<<", "<<recDosen[i].getTahunLahir()<<endl;
-					cout<< "NRP 	: "<<recDosen[i].getNPP()<<endl;
-					cout<< "Departemen	: "<<recDosen[i].getDepartemen()<<endl;
-					cout<< "Pendidikan Terakhir	: "<<recDosen[i].getPendidikan()<<endl;
-					cout << endl;
+					cout<<"\n"<<recDosen[i].getId()<< ". Nama Dosen		: "<<recDosen[i].getNama()<<endl;
+					cout<< "  NPP 			: "<<recDosen[i].getNPP()<<endl;
+					
 				}
+				cout<< "masukkan nomer ID yang ingin dilihat : ";
+				cin>> noID;
+					cout<<"\n\n"<<recDosen[noID-1].getId()<< ". Nama Dosen		: "<<recDosen[noID-1].getNama()<<endl;
+					cout<< "  Tanggal Lahir		: "<<recDosen[noID-1].getTglLahir()<< ", "<< recDosen[noID-1].getBulanLahir()<<", "<<recDosen[noID-1].getTahunLahir()<<endl;
+					cout<< "  NPP 			: "<<recDosen[noID-1].getNPP()<<endl;
+					cout<< "  Departemen		: "<<recDosen[noID-1].getDepartemen()<<endl;
+					cout<< "  Pendidikan Terakhir	: "<<recDosen[noID-1].getPendidikan()<<endl;
+					cout << endl;
 			}
 				break;
 
@@ -160,22 +172,26 @@ int main(int argc, char** argv)
 			{
 				for (int i = 0; i < recMhs.size(); i++)
 				{
-					cout<< "Nama Mahasiswa	: "<<recTendik[i].getNama()<<endl;
-					cout<< "Tanggal Lahir	: "<<recTendik[i].getTglLahir()<< ", "<< recTendik[i].getBulanLahir()<<", "<<recTendik[i].getTahunLahir()<<endl;
-					cout<< "NRP 	: "<<recTendik[i].getNPP()<<endl;
-					cout<< "Tahun Masuk	: "<<recTendik[i].getUnit()<<endl;
-					cout << endl;
+					cout<<"\n"<<recTendik[i].getId()<< ". Nama Tendik	: "<<recTendik[i].getNama()<<endl;
+					cout<< "  NPP 		: "<<recTendik[i].getNPP()<<endl;
 				}
+				cout<< "masukkan nomer ID yang ingin dilihat : ";
+				cin>> noID;
+					cout<<"\n\n"<<recTendik[noID-1].getId()<< ". Nama Tendik	: "<<recTendik[noID-1].getNama()<<endl;
+					cout<< "  NPP 			: "<<recTendik[noID-1].getNPP()<<endl;
+					cout<< "  Tanggal Lahir		: "<<recTendik[noID-1].getTglLahir()<< ", "<< recTendik[noID-1].getBulanLahir()<<", "<<recTendik[noID-1].getTahunLahir()<<endl;
+					cout<< "  Tahun Masuk		: "<<recTendik[noID-1].getUnit()<<endl;
+					cout << endl;
 			}
 			break;
-
+			case 7: return 0;
 			default: 
 				cout<< "Input yang dipilihan salah, Silahkan  Masukkan Kembali\n";
-				cout<< "Untuk kembali ke menu utama masukkan angka 1  ";
-				cin>>pilihan;
-				
 				break;
+			
 		}
+			
+			cin.ignore();
 	}
 
 	return 0;
